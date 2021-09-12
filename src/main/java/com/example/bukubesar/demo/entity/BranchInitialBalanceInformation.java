8f8f469a9f3323892e7da7f4dcb234c5654e17b2
ekawinata157 <wintaeka@yahoo.com>
2021-09-12 13:16:55 +0700

    [Eka] Declare entity relation in BranchInitialBalanceInformation.java

--- src/main/java/com/example/bukubesar/demo/entity/BranchInitialBalanceInformation.java
+++ src/main/java/com/example/bukubesar/demo/entity/BranchInitialBalanceInformation.java
@@ -1,4 +1,21 @@
 package com.example.bukubesar.demo.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "branch_initial_balance")
+@NoArgsConstructor
 public class BranchInitialBalanceInformation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String branch;
+    private String period;
+    private String code;
+    private double debit;
+    private double credit;
 }
